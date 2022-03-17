203f41aa7e664914e08c3f275d12f2f592619325
Leon <l.beccard@protonmail.com>
2022-03-17 14:09:36 +0100

    Add HTML lines for download button

--- assets/js/templates/main.js
+++ assets/js/templates/main.js
@@ -24,15 +24,15 @@ function createTemplate_main({ chartID, chartTitle, chartDescription, chartSourc
 
             <button class="chart-btn" onclick="shareChartTwitter('${chartID}')">
                 <i class="fa fa-twitter"></i>Twitter
-            </button> 
+            </button>
 
             <button class="chart-btn"><i class="fas fa-facebook"></i>Facebook</button>
 
             <button class="chart-btn" onclick="copyChartURL('${chartID}')">
                 <i class="fas fa-link"></i>Copy link
-            </button> 
+            </button>
 
-        </div> 
+        </div>
 
         <button onclick="toggleArea(this, 'save', '${chartID}')" value="OFF"  class="chart-btn">
                 <i class="fas fa-arrow-alt-circle-down"></i>Download
@@ -46,11 +46,15 @@ function createTemplate_main({ chartID, chartTitle, chartDescription, chartSourc
             </button>
             </a>
 
-            <button class="chart-btn">
+            <button onclick="downloadImage('${chartID}', '${chartTitle}', '${chartDescription}', '${chartSources}')" class="chart-btn">
                 <i class="fas fa-arrow-alt-circle-down"></i>Picture (png)
             </button>
 
-        </div> 
+            <div class="downloadImage" id="downloadImage-${chartID}" >
+
+            </div>
+
+        </div>
 
         <button value="OFF"  class="chart-btn">
                 <i class="fas fa-info"></i>Sources
