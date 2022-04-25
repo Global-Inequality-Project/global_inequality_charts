856df5f7666ad11243bc2d5f8613f2a7fd2010c9
Leon <l.beccard@protonmail.com>
2022-04-25 16:04:40 +0200

    Fix y-axis labels, change function name

--- charts/poverty_num_min_china/poverty_num_min_china.js
+++ charts/poverty_num_min_china/poverty_num_min_china.js
@@ -5,12 +5,12 @@ jQuery(function() {
       years: { start: 1981, end: 2015},
   };
 
-  importFilesAndShow_inequality_gdp_nrt_sth();
+  importFilesAndShow_poverty_num_min_china();
 
 });
 
 //-------------------------------------- importFilesAndShow
-function importFilesAndShow_inequality_gdp_nrt_sth(){
+function importFilesAndShow_poverty_num_min_china(){
 
   var chartID = "poverty_num_min_china"
 
@@ -55,7 +55,7 @@ function render_poverty_num_min_china(canvasID){
                   yaxis: {
                       tickAmount: 5,
                       labels: {
-                          formatter: (val, index) => '$'+formatYAxisLabel(val, index, 0, true)
+                          formatter: (val, index) => formatYAxisLabel(val, index, 0, true)
                       }
                   },
               }
@@ -66,7 +66,7 @@ function render_poverty_num_min_china(canvasID){
           width: 2.5
       },
       yaxis: {
-          labels: { formatter: (val, index) => '$'+formatYAxisLabel(val, index, 0, true) }
+          labels: { formatter: (val, index) => formatYAxisLabel(val, index, 1, true) }
       },
       colors: ['#775DD0', '#FF4560', '#FEB019', '#00E396', '#008FFB', '#A5978B'],
       grid: {
@@ -75,7 +75,7 @@ function render_poverty_num_min_china(canvasID){
           }
       },
       legend: { fontSize: '12px', markers: { width: 10, height: 10, radius: 10, offsetY: '-2px'}},
-      tooltip: { y: { formatter: (val, index) => '$'+formatTooltipVal(val, index) }}
+      tooltip: { y: { formatter: (val, index) => formatTooltipVal(val, index) }}
 }
 
 let div_id = '#poverty_num_min_china';
@@ -83,13 +83,13 @@ let div_id = '#poverty_num_min_china';
   let data_hash  = makeHash(data, 'year');
 
 let years = [];
-for (let year=window.chart_data["poverty_num_min_china"].years.start, end = window.chart_data["inequality_gdp_nrt_sth"].years.end; year <= end; ++year)
+for (let year=window.chart_data["poverty_num_min_china"].years.start, end = window.chart_data["poverty_num_min_china"].years.end; year <= end; ++year)
   years.push(year);
 
 let series = [
-      { name: '7.5', data:[]},
-      { name: '10', data:[]},
-      { name: '15', data:[]}
+      { name: '7.5$/day', data:[]},
+      { name: '10$/day', data:[]},
+      { name: '15$/day', data:[]}
   ];
 
   years.forEach(year => {
@@ -118,15 +118,15 @@ function abbreviatedTooltip(dataPointIndex, w){
   let year = w.globals.categoryLabels[dataPointIndex];
   let color1 = w.config.colors[0];
   let marker1 = '<div class="custom-tooltip-marker" style="background-color:'+color1+'"></div>';
-  let val1 = '$'+formatYAxisLabel(config_series[0].data[dataPointIndex], null, 0, true)
+  let val1 = formatYAxisLabel(config_series[0].data[dataPointIndex], null, 0, true)
 
   let color2 = w.config.colors[1];
   let marker2 = '<div class="custom-tooltip-marker" style="background-color:'+color2+'"></div>';
-  let val2 = '$'+formatYAxisLabel(config_series[1].data[dataPointIndex], null, 0, true)
+  let val2 = formatYAxisLabel(config_series[1].data[dataPointIndex], null, 0, true)
 
   let color3 = w.config.colors[2];
   let marker3 = '<div class="custom-tooltip-marker" style="background-color:'+color3+'"></div>';
-  let val3 = '$'+formatYAxisLabel(config_series[2].data[dataPointIndex], null, 0, true)
+  let val3 = formatYAxisLabel(config_series[2].data[dataPointIndex], null, 0, true)
 
   return '<div class="custom-tooltip-box">' +
       '<div class="custom-tooltip-title">' + year+ '</div>' +
