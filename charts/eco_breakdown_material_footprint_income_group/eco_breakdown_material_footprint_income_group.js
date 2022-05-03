0ed280486d6ff759597c8ab77311b6e7c08e2604
Leon <l.beccard@protonmail.com>
2022-05-03 13:31:49 +0200

    Add y-axis annotation as sustainability threshold

--- charts/eco_breakdown_material_footprint_income_group/eco_breakdown_material_footprint_income_group.js
+++ charts/eco_breakdown_material_footprint_income_group/eco_breakdown_material_footprint_income_group.js
@@ -83,6 +83,22 @@ function render_eco_breakdown_material_footprint_income_group(canvasID) {
         legend: {
             show: false
         },
+        annotations: {
+            yaxis: [
+                {
+                y: 7.7,
+                borderColor: '#00E396',
+                label: {
+                    borderColor: '#00E396',
+                    style: {
+                        color: '#fff',
+                        background: '#00E396'
+                    },
+                        text: 'Sustainability threshold'
+                }
+                }
+            ]
+        }
 	}
 
 	let data = window.chart_data["eco_breakdown_material_footprint_income_group"].data;
